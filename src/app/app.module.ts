cc44b3957f40dfb488b4ce5da7d2a8ce80a9683c
Anooshik Vartanian <vartanian.anooshik@gmail.com>
2022-09-30 18:23:14 +0400

    tour of heroes angular tutorial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule,
+	 HttpClientModule,
+	 HttpClientInMemoryWebApiModule.forRoot(
+  InMemoryDataService, { dataEncapsulation: false }
+),
+ AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero {
+  id: number;
+  name: string;
+  link: string;
+  quote: string;
+  img: string;
+  description:string;
+}
